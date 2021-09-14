51689797de79d9f67ef60f1d47f8474a973be954
I Lin WU <ilinwu@Is-MacBook-Air.broadband>
2021-09-14 12:40:14 +0100

    set server url for the client

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: '/graphql',
   cache: new InMemoryCache()
 });
 
